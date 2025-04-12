75409e35cb0f0fe993f5ea6c71d8be8b602b846f
Dythia Prayudhatama <dythia102@gmail.com>
2025-04-12 11:11:35 +0700

    Front End Development Setup

---
+++ frontend/next.config.js
@@ -0,0 +1,11 @@
+// next.config.js
+module.exports = {
+    webpackDevMiddleware: config => {
+      config.watchOptions = {
+        poll: 1000, // Check for changes every second
+        aggregateTimeout: 300,
+      };
+      return config;
+    },
+  };
+  
