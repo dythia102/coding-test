176d8b8f4e01d1e234fa1203dcff19b5e28ff41c
Dythia Prayudhatama <dythia102@gmail.com>
2025-05-07 00:11:47 +0700

    Refactor Jenkins configuration: update Dockerfile base image, add start and stop scripts, and unify job names in Jenkins configuration

---
+++ devops/devop-start.sh
@@ -0,0 +1,1 @@
+docker compose -f docker-compose.devops.yml up --build -d
--- devops/devops-stop.sh
+++ devops/devops-stop.sh
@@ -1,3 +1,2 @@
 docker compose -f docker-compose.devops.yml down -v
 sudo rm -rf ./jenkins/jenkins_home/*
-docker compose -f docker-compose.devops.yml up --build -d
