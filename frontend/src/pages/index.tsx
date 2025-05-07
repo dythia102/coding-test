55913efa71afae1401fbfbeca64dac0d8b4f4945
dythia102 <dythia102@gmail.com>
2025-05-08 03:16:37 +0700

    Update index.tsx

--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -74,7 +74,7 @@ export default function Home() {
   return (
     <Container maxWidth="md" sx={{ py: 4 }}>
       <Typography variant="h4" gutterBottom color="primary">
-        Sales Dashboard Prod Pipeline test 2
+        Sales Dashboard Prod Pipeline test 7
       </Typography>
 
       <Paper elevation={2} sx={{ p: 2, mb: 4 }}>
